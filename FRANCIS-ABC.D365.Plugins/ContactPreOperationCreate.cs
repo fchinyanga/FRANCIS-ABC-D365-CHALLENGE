68b0f8e3a13c9870782af1c8169add41d11c9b9a
Francis Chinyanga <fchinyanga@singular.co.za>
2021-04-15 08:34:21 +0200

    re-used the plugin base and called the pre create method in contact manager class

--- FRANCIS-ABC.D365.Plugins/ContactPreOperationCreate.cs
+++ FRANCIS-ABC.D365.Plugins/ContactPreOperationCreate.cs
@@ -1,10 +1,9 @@
 ﻿
-namespace Francis_ABC.D365.Plugins.Plugins
+namespace Francis_ABC.D365.Plugins
 {
-
-  using Francis_ABC.D365.Core.Helpers.Plugins.Create;
-  using Francis_ABC.D365.Core.Managers;
-  using Francis_ABC.D365.Entities;
+  using Francis_ABC.D365.Plugins.Core.Helpers.Plugins.Create;
+  using Francis_ABC.D365.Plugins.Core.Managers;
+  using Francis_ABC.D365.Plugins.Entities;
 
   [CrmPluginRegistration(
     MessageNameEnum.Create,
@@ -12,13 +11,15 @@ namespace Francis_ABC.D365.Plugins.Plugins
     StageEnum.PreOperation,
     ExecutionModeEnum.Synchronous,
     "",
-    "Francis_ABC.D365.Plugins.Plugins.ContactPreOperationCreate",
+    "Francis_ABC.D365.Plugins.ContactPreOperationCreate",
     1,
     IsolationModeEnum.Sandbox,
     Id = "53c1f0cc-0a36-ea11-a813-000d3ab3626x",
-    Description = "Francis_ABC.D365.Plugins.Plugins.ContactPreOperationCreate")]
+    Description = "Francis_ABC.D365.Plugins.ContactPreOperationCreate")]
   public class ContactPreOperationCreate : PluginBase
   {
+
+
     public ContactPreOperationCreate(string unsecure, string secure)
         : base(typeof(ContactPreOperationCreate))
     {
@@ -26,74 +27,10 @@ namespace Francis_ABC.D365.Plugins.Plugins
 
     protected override void ExecuteCrmPlugin(LocalPluginContext localContext)
     {
+      localContext.Trace($"Entered my method .Execute()");
       Contact contact = InputParameters.GetTarget(localContext.PluginExecutionContext).ToEntity<Contact>();
       ContactManager.SetValuesForReadOnlyFields(localContext.OrganizationService, localContext.TracingService, contact);
     }
   }
 
-  /*public class ContactPreOperationCreate : IPlugin
-  {
-    public void Execute(IServiceProvider serviceProvider)
-    {
-      try
-      {
-        IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
-        if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
-        {
-          ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-          IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-          IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
-
-          if (context.MessageName.ToLower() == "create" && context.Stage == 20)
-          {
-            Entity targetEntity = context.InputParameters["Target"] as Entity;
-            int age = 0;
-            double amountInvested = 0.0;
-            int investmentPeriod = 0;
-            double interestRate = 0.0;
-            double estimatedReturnValue = 0;
-            DateTime maturityDate = DateTime.Now;
-            DateTime joiningDate = DateTime.Now;
-            DateTime dob = DateTime.Now;
-
-            if (targetEntity.LogicalName.ToLower() != "abc_contact")
-            {
-              return;
-            }
-            age = calculateAge(dob);
-            targetEntity["abc_age"] = age;
-
-            maturityDate = calculateMaturityDate(joiningDate, investmentPeriod);
-            targetEntity["abc_maturitydate"] = maturityDate;
-
-            estimatedReturnValue = calculateEstimatedReturnValue(joiningDate, amountInvested, investmentPeriod, interestRate);
-            targetEntity["abc_estimatedreturnvalue"] = estimatedReturnValue;
-
-            //optionset
-            // targetEntity["abc_statusreason"] = estimatedReturnValue;
-          }
-        }
-      }
-      catch (Exception ex)
-      {
-        throw new InvalidPluginExecutionException(ex.Message);
-      }
-
-    }
-
-    private double calculateEstimatedReturnValue(DateTime joiningDate, double amountInvested, int investmentPeriod, double interestRate)
-    {
-      return 1000000.00;
-    }
-
-    private DateTime calculateMaturityDate(DateTime joiningDate, int investmentPeriod)
-    {
-      return DateTime.Now.AddYears(10);
-    }
-
-    private int calculateAge(DateTime? dob)
-    {
-      return 50;
-    }
-  }*/
 }

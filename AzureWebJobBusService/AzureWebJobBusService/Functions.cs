d5a9907d9ba86baa952272488a24b6a95a45212d
Francis Chinyanga <fchinyanga@singular.co.za>
2021-04-28 08:11:32 +0200

    Added an azure web job project

---
+++ AzureWebJobBusService/AzureWebJobBusService/Functions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Xrm.Sdk;
+
+namespace AzureWebJobBusService
+{
+  public class Functions
+  {
+    // This function will get triggered/executed when a new message is written 
+    // on an Azure Queue called queue.
+    /*  public static void processservicebus(
+      [ServiceBusTrigger("messages")]string myQueueItem,
+      ILogger log)
+      {
+        log.LogInformation(myQueueItem);
+      }*/
+
+    public static void processservicebus(
+   [ServiceBusTrigger("messages")]RemoteExecutionContext remoteExecutionContext,
+   ILogger log)
+    {
+      log.LogInformation("===================Message Start========================");
+      log.LogInformation(remoteExecutionContext.UserId.ToString());
+      log.LogInformation("===================Message End========================");
+    }
+  }
+}
---
+++ AzureWebJobBusService/AzureWebJobBusService/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AzureWebJobBusService
+{
+  // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+  class Program
+  {
+    // Please set the following connection strings in app.config for this WebJob to run:
+    // AzureWebJobsDashboard and AzureWebJobsStorage
+    static void Main()
+    {
+      var builder = new HostBuilder();
+      var connString = "";
+      builder.ConfigureWebJobs(b =>
+       {
+         b.AddAzureStorageCoreServices();
+         b.AddServiceBus(c => c.ConnectionString = connString);
+       });
+      builder.ConfigureLogging((context, b) =>
+      {
+        b.AddConsole();
+      });
+      var host = builder.Build();
+      using (host)
+      {
+        host.Run();
+      }
+    }
+  }
+}

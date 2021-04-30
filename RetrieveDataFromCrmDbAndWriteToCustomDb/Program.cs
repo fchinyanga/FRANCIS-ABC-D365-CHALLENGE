ce8b2455e409e44cf26c80e7eb3ca0f70c70b573
Francis Chinyanga <fchinyanga@singular.co.za>
2021-04-30 15:20:11 +0200

    Basic Console app to connect to CRM online

---
+++ RetrieveDataFromCrmDbAndWriteToCustomDb/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RetrieveDataFromCrmDbAndWriteToCustomDb.custom;
+
+namespace RetrieveDataFromCrmDbAndWriteToCustomDb
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        RetrieveWriteToDb.getContactRecords();
+        Console.Write("Doc found");
+      }
+      catch (Exception ex)
+      {
+        Console.Write(ex.Message);
+      }
+    }
+  }
+}

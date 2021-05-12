ee2d83fb4c6e7275ebcbc3688815b666cee5070b
Francis Chinyanga <fchinyanga@singular.co.za>
2021-05-12 14:59:18 +0200

    Auto stash before checking out "origin/master"

---
+++ ReadEntityRelationshipTreeFromXMLFile/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Xrm.Tooling.Connector;
+using ReadEntityRelationshipTreeFromXMLFile.config;
+using ReadEntityRelationshipTreeFromXMLFile.datatranscations;
+
+namespace ReadEntityRelationshipTreeFromXMLFile
+{
+  class Program
+  {
+    private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    static void Main(string[] args)
+    {
+      var watch = System.Diagnostics.Stopwatch.StartNew();
+      var settingsFilePath = "appsettings.json";
+      var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(settingsFilePath, optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+      IServiceCollection services = new ServiceCollection();
+      services.AddSingleton<ConfigSettings>(config.GetSection("ConfigSettings").Get<ConfigSettings>());
+
+      try
+      {
+        log.Info("\n===============Programme started =====================");
+        log.Info("\n===============Reading from xml file =====================");
+        CrmServiceClient crmServiceClient = new CrmServiceClient(ConfigSettings.crmConnectionString);
+        if (crmServiceClient.IsReady)
+        {
+          Console.WriteLine("======================== Connection successful ===============================");
+          XmlReaderSettings settings = new XmlReaderSettings();
+          settings.Async = true;
+          using (XmlReader reader = XmlReader.Create(ConfigSettings.entityTreeFilePath, settings))
+          {
+            while (reader.Read())
+            {
+              switch (reader.NodeType)
+              {
+                case XmlNodeType.Element:
+                  // Console.WriteLine("Start Element = {0}", reader.Name);
+                  break;
+                case XmlNodeType.Text:
+                  var entityName = "" + reader.ReadContentAsString().Trim().ToString();
+                  log.Info($"\n\tText Node = " + entityName);
+                  if (entityName.Trim().Equals("Contact"))
+                  {
+
+                    Console.WriteLine("===============Retrive Contact from db================");
+                    for (int i = 0; i < 500; i++)
+                    {
+                      ContactArchival.ArchiveContact(crmServiceClient, ConfigSettings.sqlServerDbConnectionString);
+                    }
+                  }
+                  break;
+                case XmlNodeType.EndElement:
+                  // Console.WriteLine("====== End Element  {0} =======", reader.Name);
+                  break;
+                default:
+                  // Console.WriteLine("Other node {0} with value {1}", reader.NodeType, reader.Value);
+                  break;
+              }
+            }
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+
+        Console.WriteLine("Exception caught here " + ex.ToString());
+      }
+      //log.Error("\n\t\tError encountered", new IndexOutOfRangeException());
+      watch.Stop();
+      var elapsedMs = watch.ElapsedMilliseconds;
+      log.Info("\n\n========Finished the whole process in : " + elapsedMs + "ms ==================");
+      Console.ReadKey();
+    }
+  }
+}
---
+++ ReadEntityRelationshipTreeFromXMLFile/datatranscations/ContactArchival.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace ReadEntityRelationshipTreeFromXMLFile.datatranscations
+{
+  public class ContactArchival
+  {
+
+    private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    public static void ArchiveContact(CrmServiceClient crmServiceClient, string connStr)
+    {
+      var watch = System.Diagnostics.Stopwatch.StartNew();
+      log.Info("======================== Connection successful ===============================");
+      string createQuery = "ContactInsertCommand";
+      string checkIfContactExist = "CheckIfContactExist";
+      string updateContact = "ContactUpdateCommand";
+      QueryExpression query = new QueryExpression("contact");
+      query.ColumnSet = new ColumnSet(true);
+      // query.Criteria = new FilterExpression();
+      // query.Criteria.AddCondition("abc_clientage", ConditionOperator.GreaterEqual, 0);
+
+      EntityCollection enColl = crmServiceClient.RetrieveMultiple(query);
+      Console.WriteLine("================ The number of records is: " + enColl.Entities.Count.ToString() + " ===================\n\n");
+
+      Guid ContactId = new Guid();
+      SqlConnection con = new SqlConnection(connStr);
+      con.Open();
+      for (int i = 0; i < enColl.Entities.Count; i++)
+      {
+        #region variables
+        int? age = null, investmentPeriod = null, statusCode = 0, stateCode = 0;
+        DateTime? dob = null, maturityDate = null, joiningDate = null;
+        decimal? interesRate = null;
+        string corporateCliName = "", individualCliName = "";
+        Money initialInvestment = null, estimatedReturn = null;
+        #endregion
+        #region check columns
+        var estReturn = enColl.Entities[i].Attributes.ContainsKey("abc_clientage") ? 10 : 200;
+        log.Info("=========================== Record " + i.ToString() + " ===================================================");
+        if (enColl.Entities[i].Attributes.ContainsKey("contactid"))
+        {
+          ContactId = (Guid)enColl.Entities[i].Attributes["contactid"];
+        }
+        if (enColl.Entities[i].Attributes.ContainsKey("abc_clientage"))
+        {
+          age = (int)enColl.Entities[i].Attributes["abc_clientage"];
+        }
+
+        if (enColl.Entities[i].Attributes.ContainsKey("abc_estimatedreturn"))
+        {
+          estimatedReturn = (Money)enColl.Entities[i].Attributes["abc_estimatedreturn"];
+        }
+        if (enColl.Entities[i].Attributes.ContainsKey("abc_corporateclientname"))
+        {
+          corporateCliName = (string)enColl.Entities[i].Attributes["abc_corporateclientname"];
+        }
+        if (enColl.Entities[i].Attributes.ContainsKey("abc_individualclientname"))
+        {
+          individualCliName = (string)enColl.Entities[i].Attributes["abc_individualclientname"];
+        }
+        if (enColl.Entities[i].Attributes.ContainsKey("abc_initialinvestment"))
+        {
+          initialInvestment = (Money)enColl.Entities[i].Attributes["abc_initialinvestment"];
+        }
+        if (enColl.Entities[i].Attributes.ContainsKey("abc_interestrate"))
+        {
+          interesRate = (decimal)enColl.Entities[i].Attributes["abc_interestrate"];
+        }
+        if (enColl.Entities[i].Attributes.ContainsKey("abc_investmentperiod"))
+        {
+          investmentPeriod = (int)enColl.Entities[i].Attributes["abc_investmentperiod"];
+        }
+        if (enColl.Entities[i].Attributes.ContainsKey("abc_joiningdate"))
+        {
+          joiningDate = (DateTime)enColl.Entities[i].Attributes["abc_joiningdate"];
+        }
+        if (enColl.Entities[i].Attributes.ContainsKey("abc_maturitydate"))
+        {
+          maturityDate = (DateTime)enColl.Entities[i].Attributes["abc_maturitydate"];
+        }
+        if (enColl.Entities[i].Attributes.ContainsKey("abc_dateofbirth"))
+        {
+          dob = (DateTime)enColl.Entities[i].Attributes["abc_dateofbirth"];
+        }
+
+        if (enColl.Entities[i].Attributes.ContainsKey("statuscode"))
+        {
+          statusCode = (int)((OptionSetValue)enColl.Entities[i].Attributes["statuscode"]).Value;
+        }
+
+        if (enColl.Entities[i].Attributes.ContainsKey("statecode"))
+        {
+          stateCode = (int)((OptionSetValue)enColl.Entities[i].Attributes["statecode"]).Value;
+        }
+        #endregion
+
+        SqlCommand createCommand = new SqlCommand(createQuery, con);
+        SqlCommand checkIfContactExistCommand = new SqlCommand(checkIfContactExist, con);
+        SqlCommand upadteExistingContactCommand = new SqlCommand(updateContact, con);
+
+        #region check if contact exist
+        checkIfContactExistCommand.CommandType = CommandType.StoredProcedure;
+        checkIfContactExistCommand.Parameters.AddWithValue("@ContactId", ContactId);
+        var returnParameter = checkIfContactExistCommand.Parameters.Add("@Exist", SqlDbType.Int);
+        returnParameter.Direction = ParameterDirection.ReturnValue;
+        checkIfContactExistCommand.ExecuteNonQuery();
+        var result = returnParameter.Value;
+        #endregion
+
+        if ((int)result == 0)
+        {
+          #region create contact
+          try
+          {
+
+            createCommand.CommandType = CommandType.StoredProcedure;
+            createCommand.Parameters.AddWithValue("@ContactId", ContactId);
+            createCommand.Parameters.AddWithValue("@abc_clientage", age);
+            createCommand.Parameters.AddWithValue("@abc_dateofbirth", dob);
+            createCommand.Parameters.AddWithValue("@abc_estimatedreturn", estimatedReturn?.Value);
+            createCommand.Parameters.AddWithValue("@abc_initialinvestment", initialInvestment?.Value);
+            createCommand.Parameters.AddWithValue("@abc_interestrate", interesRate);
+            createCommand.Parameters.AddWithValue("@abc_investmentperiod", investmentPeriod);
+            createCommand.Parameters.AddWithValue("@abc_joiningdate", joiningDate);
+            createCommand.Parameters.AddWithValue("@abc_maturitydate", maturityDate);
+            createCommand.Parameters.AddWithValue("@statuscode", statusCode);
+            createCommand.Parameters.AddWithValue("@statecode", stateCode);
+            createCommand.Parameters.AddWithValue("@abc_corporateclientname", corporateCliName);
+            createCommand.Parameters.AddWithValue("@abc_individualclientname", individualCliName);
+            createCommand.ExecuteNonQuery();
+            log.Info($"ContactId={ContactId}\t Client age= {age}\t" +
+            $"Estimated Return={estimatedReturn?.Value} \t" +
+            $"Coorp Client Name= {corporateCliName}\t" +
+            $"Ind Client Name={individualCliName}\t" +
+            $"DOB={dob}\t" +
+            $"Initial Invetment={initialInvestment?.Value}\t" +
+            $"Interest Rate={interesRate}\t" +
+            $"Investment Period={investmentPeriod}\t" +
+            $"Joining Date= {joiningDate}\t" +
+            $"Maturity Date={maturityDate}\t"
+           );
+          }
+          catch (Exception ex)
+          {
+            log.Info("Record for \t" + ContactId + "\t was not recorded into the database because of \t" + ex.Message);
+          }
+          #endregion
+        }
+        else
+        {
+          #region update contact
+          log.Info("\tContact with contactId\t" + ContactId + "\t already exist and will be updated");
+          try
+          {
+
+            upadteExistingContactCommand.CommandType = CommandType.StoredProcedure;
+            upadteExistingContactCommand.Parameters.AddWithValue("@ContactId", ContactId);
+            upadteExistingContactCommand.Parameters.AddWithValue("@abc_clientage", age);
+            upadteExistingContactCommand.Parameters.AddWithValue("@abc_dateofbirth", dob);
+            upadteExistingContactCommand.Parameters.AddWithValue("@abc_estimatedreturn", estimatedReturn?.Value);
+            upadteExistingContactCommand.Parameters.AddWithValue("@abc_initialinvestment", initialInvestment?.Value);
+            upadteExistingContactCommand.Parameters.AddWithValue("@abc_interestrate", interesRate);
+            upadteExistingContactCommand.Parameters.AddWithValue("@abc_investmentperiod", investmentPeriod);
+            upadteExistingContactCommand.Parameters.AddWithValue("@abc_joiningdate", joiningDate);
+            upadteExistingContactCommand.Parameters.AddWithValue("@abc_maturitydate", maturityDate);
+            upadteExistingContactCommand.Parameters.AddWithValue("@Original_ContactId", ContactId);
+            upadteExistingContactCommand.Parameters.AddWithValue("@statuscode", statusCode);
+            upadteExistingContactCommand.Parameters.AddWithValue("@statecode", stateCode);
+            upadteExistingContactCommand.Parameters.AddWithValue("@abc_corporateclientname", corporateCliName);
+            upadteExistingContactCommand.Parameters.AddWithValue("@abc_individualclientname", individualCliName);
+
+            upadteExistingContactCommand.ExecuteNonQuery();
+
+          }
+          catch (Exception ex)
+          {
+            log.Info("Record for \t" + ContactId + "\t was not updated into the database because of \t" + ex.Message);
+          }
+          #endregion
+        }
+        /*if (i == 2)
+        {
+          crmServiceClient.Delete("contact", ContactId);
+        }*/
+      }
+      con.Close();
+      watch.Stop();
+      var elapsedMs = watch.ElapsedMilliseconds;
+      log.Info("\n\n======== Retrieved and Write to database in: " + elapsedMs + "ms ==================");
+    }
+  }
+}
